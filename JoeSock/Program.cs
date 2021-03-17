a5c18c248e520d977ff9a77599f011909f32e14c
JoakimHoegaas <62018628+JoakimHoegaas@users.noreply.github.com>
2021-03-17 16:03:25 +0100

    Initial commit

---
+++ JoeSock/Program.cs
@@ -0,0 +1,20 @@
+﻿    using System;
+
+namespace JoeSock
+{
+    class Program
+    {
+        // https://edabit.com/challenge/C6wN5vGodWvWL7ZaK
+        // SOLVED
+        // How's the performance on this one?
+        // First time I ever used algebra in "real life"
+
+        static void Main(string[] args)
+        {
+            var socks = new SockCalc("CABBACCC");
+            var countedSocks = socks.CountSocks();
+            var countedSockPairs = countedSocks.CountPairs();
+            Console.WriteLine(countedSockPairs);
+        }
+    }
+}
---
+++ JoeSock/SockCalc.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JoeSock
+{
+    public class SockCalc
+    {
+        public string _socks;
+        public int[] Socks = new int[] {0,0,0};
+        
+        public SockCalc(string socks)
+        {
+            _socks = socks; // "CABBACCC"
+        }
+
+        public SockCalc CountSocks()
+        {
+            foreach(var x in _socks)
+            {
+                if (x == 'A') Socks[0]++;
+                if (x == 'B') Socks[1]++;
+                if (x == 'C') Socks[2]++;
+            }
+            return this;
+        }
+
+        public int CountPairs()
+        {
+            int x = 0;
+            foreach(var socks in Socks)
+            {
+                if (socks % 2 == 0) x += socks / 2;
+                else x += (socks - 1) / 2;
+            }
+            return x;
+        }
+    }
+}   
